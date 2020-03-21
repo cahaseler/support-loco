9866e005e5a65f8f3799472f100663d700227aa9
cahaseler <cahaseler@gmail.com>
2020-03-21 14:37:39 -0400

    added bulma

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "rbx/index.css";
+import { Content } from "rbx";
+
+
+
+    function App() {
+        return (
+
+          <Content>
+            Hello world from <a href="https://github.com/dfee/rbx">rbx</a>!
+          </Content>
+
+        );
+      }
 
 export default App;
